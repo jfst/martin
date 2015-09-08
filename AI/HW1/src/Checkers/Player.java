4ac6defc0420fbdf84945400f515fb6e3b44c0e0
Martin36 <martin.funkquist@gmail.com>
2015-09-08 21:43:45 +0200

    Försökte fixa något som ska kolla om man blir dödad efter draget men fastnade. Blev för många if-statser för att vara rätt. Har i alla fall gjort så att man kollar om det finns möjlighet till multipla hopp innan man utför draget.

--- AI/HW1/src/Checkers/Player.java
+++ AI/HW1/src/Checkers/Player.java
@@ -27,23 +27,77 @@ public class Player {
          * the best next state. This skeleton returns a random move instead.
          */
         
-/*        if (pState.isBOG()){
-        	*//**
+        if (pState.isBOG()){
+        	/**
         	 * In the beginning of the game it doesn't matter which
         	 * piece we move so therefore we can move any of the pieces
         	 * in the front row
-        	 *//*
+        	 */
             Random random = new Random();
             return lNextStates.elementAt(random.nextInt(lNextStates.size()));
         }
-*/        /**
-         * If we are able to do a jump then we must do it
+        /**
+         * Here we want to make the best move. We want to eliminate
+         * the opponents pieces by jumping over them. If we can do
+         * multiple jumps in a single move then that is the best move
+         * possible. Thus we check if any of the next states gives
+         * us a chance to make another jump.
          */
-//        for (int i = 0; i < lNextStates.size(); i++){
-//        	lNextStates.elementAt(i).;
+        if(lNextStates.elementAt(0).getMove().isJump()){
+/*
+        	int[] prioList = new int[lNextStates.size()];
+            for(int i = 0; i < lNextStates.size(); i++){
+            	Vector<GameState> temp = new Vector<GameState>();            	
+            	lNextStates.elementAt(i).findPossibleMoves(temp);
+            	if(temp.elementAt(0).getMove().isJump())
+            		return temp.elementAt(0);
+            	}
+*/			
+        	int[] prioList = new int[lNextStates.size()];
+        	for(int i = 0; i < lNextStates.size(); i++){
+        		prioList[i] = lNextStates.elementAt(i).getMove().length();
+        	}
+//        	Arrays.sort(prioList);
+        	/**
+        	 * Finds the move with the largest amount of jumps
+        	 */
+        	int largestJump = prioList[0];
+        	int index = 0;
+        	for(int i = 0; i < prioList.length; i++){
+        		if(prioList[i] > largestJump){
+        			largestJump = prioList[i];
+        			index = i;
+        		}
+        	}
+        	return lNextStates.elementAt(index);
+        }
         	
-//        }
         
+        /**
+         * If there is no jump to make we want to make a move that 
+         * does not put our piece in danger, i.e. a move that gives 
+         * the opponent the chance to jump over the piece the next round
+         */
+    	assert(lNextStates.elementAt(0).getMove().isNormal());
+    	GameState thisState;
+    	int row, col, cellNum;
+    	for (int i = 0; i < lNextStates.size(); i++){
+    		thisState = lNextStates.elementAt(i);
+    		cellNum = thisState.getMove().at(2);	//Gets the position where the piece ended up
+    		row = GameState.cellToRow(cellNum);
+    		col = GameState.cellToCol(cellNum);
+    		if (thisState.get(row+1, col-1) != Constants.CELL_INVALID){
+    			if (thisState.get(row+1, col-1) == Constants.CELL_RED){
+    				//If the cell contains the opponents color the we have to
+    				//check the opposite position
+    				if (thisState.get(row-1, col+1) == Constants.CELL_EMPTY ||
+    					thisState.get(row-1, col+1) == Constants.CELL_WHITE){
+    					//??
+    				}
+    			}
+    		}
+    	}
+//        pState.getMove()
         Random random = new Random();
         return lNextStates.elementAt(random.nextInt(lNextStates.size()));
 
